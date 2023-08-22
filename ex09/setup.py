704e0c38741ef92b38afb7272357305eca133772
yabdelgh <yassine.abdou.1998@gmail.com>
2023-08-22 22:00:09 +0100

    add ex09

---
+++ ex09/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    description='My Python Package',
+    author='Your Name',
+    packages=find_packages(),
+)
