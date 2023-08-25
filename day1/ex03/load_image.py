2389d34dee801ff3227f5195b158e3d88c46ebec
yabdelgh <yassine.abdou.1998@gmail.com>
2023-08-25 21:11:32 +0100

    added day01/ex03

---
+++ day1/ex03/load_image.py
@@ -0,0 +1,12 @@
+from PIL import Image
+from numpy import array
+
+def ft_load(path: str) -> array or None:
+    try:
+        assert isinstance(path, str) and path != '', 'Error: bad argument'
+        image = Image.open(path)
+        npImage = array(image)
+        print(f'The shape of image is {npImage.shape}')
+        return npImage
+    except Exception as e:
+        return e
---
+++ day1/ex03/zoom.py
@@ -0,0 +1,24 @@
+from load_image import ft_load
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main() -> int:
+    array = ft_load('/home/yabdelgh/Desktop/animal.jpeg')
+    print(array)
+    array = array[100:500, 450:850]
+    image = Image.fromarray(array).convert("L")
+    array = np.array(image)
+    reshapedArray = array.reshape(array.shape[0], array.shape[1], 1)
+    print(f'New shape after slicing: {reshapedArray.shape} or {array.shape}')
+    print(reshapedArray)
+    plt.imshow(image, cmap="gray")
+    plt.show()
+    return 0
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(e)
