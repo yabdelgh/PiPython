6f3873d7185abf1ddb166b6dabd2f27516f5210f
yabdelgh <yassine.abdelgh@gmail.com>
2026-07-21 16:16:36 +0100

    rotate

--- day1/ex04/rotate.py
+++ day1/ex04/rotate.py
@@ -1,19 +1,21 @@
 from PIL import Image
 from numpy import array
 import matplotlib.pyplot as plt
-import numpy as np
+
+# import numpy as np
 
 
 def main():
-    img = Image.open('/home/yabdelgh/Desktop/animal.jpeg')
-    img = img.crop((400, 100, 800, 500)).convert('L')
+    img = Image.open("animal.jpeg")
+    img = img.crop((400, 100, 800, 500)).convert("L")
     arr = array(img).reshape(img.size[0], img.size[1], 1)
-    print(f'The shape of image is: {arr.shape} or {img.size}\n{arr}')
+    print(f"The shape of image is: {arr.shape} or {img.size}\n{arr[:2, :2]}")
     tArr = arr.copy()
     for i in range(arr.shape[0]):
         for j in range(arr.shape[1]):
             tArr[j][i] = arr[i][j]
-    print(f'The shape after Transpose: {tArr.shape}\n{tArr}')
+    tArr = tArr.squeeze(axis=2)
+    print(f"The shape after Transpose: {tArr.shape}\n{tArr[:2, :2]}")
     plt.imshow(tArr, cmap="gray")
     plt.show()
 
