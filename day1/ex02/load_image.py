72ed6c2278d85fb43b4dc34b9eb14dc3aff5b417
yabdelgh <yassine.abdelgh@gmail.com>
2026-07-21 10:56:57 +0100

    fix zoom.py

--- day1/ex02/load_image.py
+++ day1/ex02/load_image.py
@@ -1,18 +1,3 @@
-# from PIL import Image
-# from numpy import array
-
-
-# def ft_load(path: str) -> array or None:
-#     try:
-#         assert isinstance(path, str) and path != "", "Error: bad argument"
-#         image = Image.open(path)
-#         npImage = array(image)
-#         print(f"The shape of image is {npImage.shape}\n{npImage}")
-#         return npImage
-#     except Exception as e:
-#         return e
-
-
 import numpy as np
 from PIL import Image
 
--- day1/ex03/zoom.py
+++ day1/ex03/zoom.py
@@ -5,20 +5,22 @@ import matplotlib.pyplot as plt
 
 
 def main() -> int:
-    array = ft_load('/home/yabdelgh/Desktop/animal.jpeg')
+    array = ft_load("animal.jpeg")
+    print(array[:2, :3])
     array = array[100:500, 450:850]
     image = Image.fromarray(array).convert("L")
     array = np.array(image)
 
     # This code is only meant to satisfy the incomprehensible expected output.
     reshapedArray = array.reshape(array.shape[0], array.shape[1], 1)
-    print(f'New shape after slicing: {reshapedArray.shape} or {array.shape}')
-    print(reshapedArray)
+    print(f"New shape after slicing: {reshapedArray.shape} or {array.shape}")
+    print(reshapedArray[:2, :3])
     # end
 
     plt.imshow(reshapedArray, cmap="gray")
     plt.show()
     return 0
+
 
 if __name__ == "__main__":
     try:
